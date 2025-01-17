fcfd9039e6bfcd1337511da5cb9cdd14772d7e37
Victoria Lara <victoriap99@gmail.com>
2025-01-17 01:48:59 -0300

    inclusão de cadastro e listar usuários

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,11 +1,12 @@
+
 function Navbar() {
     return (
         <>
             <div className='w-full flex justify-center py-4
             			   bg-indigo-900 text-white'>
             
-                <div className="container flex justify-between text-lg">
-                    Blog Pessoal
+                <div className="container flex justify-between text-lg pr-4 pl-4">
+                    Blog pessoal
 
                     <div className='flex gap-4'>
                         Postagens
