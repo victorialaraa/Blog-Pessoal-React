0b9c01b584af03904d5dca34ba693a64efe89aaf
Victoria Lara <victoriap99@gmail.com>
2025-01-27 01:02:54 -0300

    Inclusão de pagina de perfil e ajustes finais

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,53 +1,51 @@
-import { useContext } from "react";
-import { Link, useNavigate } from "react-router-dom";
+import { ReactNode, useContext } from "react";
+import { Link, useNavigate } from "react-router-dom"
 import { AuthContext } from "../../contexts/AuthContext";
+import { ToastAlerta } from "../../utils/ToastAlerta";
 
 function Navbar() {
-  const navigate = useNavigate();
 
-  const { handleLogout } = useContext(AuthContext);
+    const navigate = useNavigate();
 
-  function logout() {
-    handleLogout();
-    alert("O Usuário foi desconectado com sucesso!");
-    navigate("/");
-  }
+    const { usuario, handleLogout } = useContext(AuthContext)
 
-  return (
-    <>
-      <div
-        className='w-full flex justify-center py-4 bg-gray-800 text-gray-50'>
-        <div className="container mx-auto px-4 py-4">
-          <div className="flex items-center justify-between">
-            
+    function logout() {
 
-            <div className="flex items-center gap-2">   
+        handleLogout()
+        ToastAlerta('O Usuário foi desconectado com sucesso!', 'info')
+        navigate('/')
+    }
+    
+    let component: ReactNode
 
-              <Link to="/home" className="text-2xl font-bold">Blog Pessoal</Link>
+    if (usuario.token !== "") {
 
+        component = (
+
+            <div className='w-full flex justify-center py-4 bg-gray-800 text-gray-50'>
+
+                <div className="container flex justify-between text-lg">
+                    <Link to='/home' className="text-2xl font-bold">Blog Pessoal</Link>
+
+                    <div className='flex gap-4'>
+                        <Link to='/postagens' className='hover:underline'>Postagens</Link>
+                        <Link to='/temas' className='hover:underline'>Temas</Link>
+                        <Link to='/cadastrartema' className='hover:underline'>Cadastrar tema</Link>
+                        <Link to='/perfil' className='hover:underline'>Perfil</Link>
+                        <Link to='' onClick={logout} className='hover:underline'>Sair</Link>
+                    </div>
+                </div>
             </div>
 
+        )
 
+    }
 
-            <div className="flex gap-4">
-
-              <Link to='/postagens' className='hover:underline'>Postagens</Link>
-
-              <Link to="/temas" className="hover:underline">Temas</Link>
-
-              <Link to="/cadastrartema" className="hover:underline">Cadastrar tema</Link>
-
-              Perfil
-
-              <Link to="" onClick={logout} className="hover:underline">Sair</Link>
-
-            </div>
-
-          </div>
-        </div>
-      </div>
-    </>
-  );
+    return (
+        <>
+            { component }
+        </>
+    )
 }
 
-export default Navbar;
+export default Navbar
